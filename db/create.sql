a74f7a643c080c469bb0cd09b848a54f21f4c722
layla5HD <74540353+layla5HD@users.noreply.github.com>
2022-03-01 13:23:43 +0000

    maten toegevogd

--- db/create.sql
+++ db/create.sql
@@ -7,7 +7,8 @@ CREATE TABLE products (
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC(10, 2),
+  maten NUMERIC (3)
 );
 
 CREATE TABLE information (
@@ -27,24 +28,24 @@ CREATE TABLE information (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Look-a-like 1', 'wil je dezelfde outfit aan als... dan is deze perfect voor jou, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Look-a-like 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Look-a-like 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Look-a-like 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Look-a-like 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Look-a-like 6', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 20);
+insert into products (name, description, code, price) values ('Look-a-like 1', 'wil je dezelfde outfit aan als Taylor Swift dan is deze perfect voor jou, deze outfit heet ookwel taylors version met de kleur rood als thema, cursus id.', '816905633-0', 70);
+insert into products (name, description, code, price) values ('Look-a-like 2', 'Wil je dezelfde outfit aan als Ariana Grande dan is deze outfit perfect voor jou. In drie maten beschikbaar!.', '077030122-3', 50);
+insert into products (name, description, code, price) values ('Look-a-like 3', 'Dit zijn dezelfde accesoires als uit de serie Gossip Girl. veel goedkoper dan in de bijenkorf.', '445924201-X', 20);
+insert into products (name, description, code, price) values ('Look-a-like 4', 'Wil je dezelfde outfit aan als Justin Bieber dan is deze outfit voor jou!,Dit is een gender neutrale outfit,in 4 maten beschkibaar dus ook voor jongens.', '693155505-7', 50);
+insert into products (name, description, code, price) values ('Look-a-like 5', 'Wil jij dezelfde sneakers als de celebrities dan is dit voor jou. in verschillende kleuren beschikbaar.Van maat 38 tot maat 40 beschikbaar.', '686928463-6',70);
+insert into products (name, description, code, price) values ('Look-a-like 6', 'Wil jij dezelfde skincare als de kardashians dan is dit pakket voor jou, dermatologisch getest, voor alle huidtypes beschikbaar.', '492662523-7', 25);
 
 
 
 
 
     
-insert into information (name, description, code) values ('Look-a-like 1', '..., cursus id.', '...');
-insert into information (name, description, code) values ('Look-a-like 2', '..., cursus id.', '...');
-insert into information (name, description, code) values ('Look-a-like 3', '..., cursus id.', '...');
-insert into information (name, description, code) values ('Look-a-like 4', '..., cursus id.', '...');
-insert into information (name, description, code) values ('Look-a-like 5', '..., cursus id.', '...');
-insert into information (name, description, code) values ('Look-a-like 6', '..., cursus id.', '...');
+insert into information (name, description, code,maten) values ('Look-a-like 1', 'taylor swift', '816905633-0',2);
+insert into information (name, description, code,maten) values ('Look-a-like 2', 'ariana grande', '077030122-3',2);
+insert into information (name, description, code,maten) values ('Look-a-like 3', 'gossip girl', '445924201-X',1);
+insert into information (name, description, code,maten) values ('Look-a-like 4', 'justin bieber', '693155505-7',4);
+insert into information (name, description, code,maten) values ('Look-a-like 5', 'sneakers', '686928463-6',3);
+insert into information (name, description, code,maten) values ('Look-a-like 6', 'kardashians', '492662523-7',1);
 
 
 
