75c4da634ba62401f0e237e731d10b224dc9c2cd
layla5HD <74540353+layla5HD@users.noreply.github.com>
2022-03-06 18:22:43 +0000

    db erbij gemaakt web doet het niet

--- db/create.sql
+++ db/create.sql
@@ -10,6 +10,7 @@ CREATE TABLE products (
   price NUMERIC(10, 2),
   maten NUMERIC (3),
   ratings_id INTEGER
+  kleuren_id INTEGER
   
 );
 
@@ -18,20 +19,26 @@ CREATE TABLE ratings (
   name VARCHAR(255)
 );
 
+CREATE TABLE kleuren (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+); 
+
+  
   CREATE TABLE platforms (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
   );
  
 
-/*  
+/*
 CREATE TABLE information (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
-  );
-*/
+  );*/
+
 --
 -- populate with data
 --
@@ -41,18 +48,18 @@ CREATE TABLE information (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Taylor', 'wil je dezelfde outfit aan als Taylor Swift dan is deze perfect voor jou, deze outfit heet ookwel taylors version met de kleur rood als thema, cursus id.', '816905633-0', 70,2,3);
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Ariana', 'Wil je dezelfde outfit aan als Ariana Grande dan is deze outfit perfect voor jou. In drie maten beschikbaar!.', '077030122-3', 50,3,1);
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Gossip', 'Dit zijn dezelfde accesoires als uit de serie Gossip Girl. veel goedkoper dan in de bijenkorf.', '445924201-X', 20,1,3);
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Justin', 'Wil je dezelfde outfit aan als Justin Bieber dan is deze outfit voor jou!,Dit is een gender neutrale outfit,in 4 maten beschkibaar dus ook voor jongens.', '693155505-7', 50,4,1);
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like sneaker', 'Wil jij dezelfde sneakers als de celebrities dan is dit voor jou. in verschillende kleuren beschikbaar.Van maat 38 tot maat 40 beschikbaar.', '686928463-6',70,3,4);
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like skincare', 'Wil jij dezelfde skincare als de kardashians dan is dit pakket voor jou, dermatologisch getest, voor alle huidtypes beschikbaar.', '452662523-7', 25,1,2);
- insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like make-up', 'Wil jij dezelfde make-up als de kardashians dan is dit pakket voor jou, dermatologisch getest, voor alle huidtypes beschikbaar.', '492762523-7', 20,1,3); 
-insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like kendall', 'Wil jij dezelfde outfit als Kendall Jenner dan is dit pakket voor jou, echt door kendall gedragen, omdat dit door Kendall is gedragen is het dus ook maar in 1 maat beschikbaar.', '92662623-7', 30,1,1);
-  insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Kylie', 'Wil jij dezelfde outfit als Kylie Jenner dan is dit pakket voor jou, echt door kylie gedragen en omdat dit door Kylie is gedragen is het dus ook maar in 1 maat beschikbaar.', '49262623-7', 35,1,1);
-  insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Blair', 'Wil jij dezelfde outfit als blair waldorf dan is dit pakket voor jou, omdat deze outfit moeilijk verkrijgbaar is, is het maar in 2 maten beschikbaar.', '49662623-7', 50,2,1);
- insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like Khloe', 'Wil jij dezelfde outfit als Khloe Kardashian dan is dit pakket voor jou, echt door khloe gedragen, omdat dit door Khloe is gedragen is het dus ook maar in 1 maat beschikbaar.', '49266263-7', 45,1,1); 
-  insert into products (name, description, code, price,maten,ratings_id) values ('Look-a-like kris', 'Wil jij dezelfde outfit als Kris Jenner dan is dit pakket voor jou, echt door kris gedragen, omdat dit door Kris is gedragen is het dus ook maar in 1 maat beschikbaar.', '492662623-8', 40,1,1);
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Taylor', 'wil je dezelfde outfit aan als Taylor Swift dan is deze perfect voor jou, deze outfit heet ookwel taylors version met de kleur rood als thema, cursus id.', '816905633-0', 70,2,3,3);
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Ariana', 'Wil je dezelfde outfit aan als Ariana Grande dan is deze outfit perfect voor jou. In drie maten beschikbaar!.', '077030122-3', 50,3,2,1);
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Gossip', 'Dit zijn dezelfde accesoires als uit de serie Gossip Girl. veel goedkoper dan in de bijenkorf.', '445924201-X', 20,1,4,3);
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Justin', 'Wil je dezelfde outfit aan als Justin Bieber dan is deze outfit voor jou!,Dit is een gender neutrale outfit,in 4 maten beschkibaar dus ook voor jongens.', '693155505-7', 50,4,2,1);
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like sneaker', 'Wil jij dezelfde sneakers als de celebrities dan is dit voor jou. in verschillende kleuren beschikbaar.Van maat 38 tot maat 40 beschikbaar.', '686928463-6',70,3,5,4);
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like skincare', 'Wil jij dezelfde skincare als de kardashians dan is dit pakket voor jou, dermatologisch getest, voor alle huidtypes beschikbaar.', '452662523-7', 25,1,5,2);
+ insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like make-up', 'Wil jij dezelfde make-up als de kardashians dan is dit pakket voor jou, dermatologisch getest, voor alle huidtypes beschikbaar.', '492762523-7', 20,1,5,3); 
+insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like kendall', 'Wil jij dezelfde outfit als Kendall Jenner dan is dit pakket voor jou, echt door kendall gedragen, omdat dit door Kendall is gedragen is het dus ook maar in 1 maat beschikbaar.', '92662623-7', 30,1,1,1);
+  insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Kylie', 'Wil jij dezelfde outfit als Kylie Jenner dan is dit pakket voor jou, echt door kylie gedragen en omdat dit door Kylie is gedragen is het dus ook maar in 1 maat beschikbaar.', '49262623-7', 35,1,2,1);
+  insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Blair', 'Wil jij dezelfde outfit als blair waldorf dan is dit pakket voor jou, omdat deze outfit moeilijk verkrijgbaar is, is het maar in 2 maten beschikbaar.', '49662623-7', 50,2,2,1);
+ insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like Khloe', 'Wil jij dezelfde outfit als Khloe Kardashian dan is dit pakket voor jou, echt door khloe gedragen, omdat dit door Khloe is gedragen is het dus ook maar in 1 maat beschikbaar.', '49266263-7', 45,1,1,1); 
+  insert into products (name, description, code, price,maten,kleuren_id,ratings_id) values ('Look-a-like kris', 'Wil jij dezelfde outfit als Kris Jenner dan is dit pakket voor jou, echt door kris gedragen, omdat dit door Kris is gedragen is het dus ook maar in 1 maat beschikbaar.', '492662623-8', 40,1,2,1);
 
 
   
@@ -62,7 +69,13 @@ insert into products (name, description, code, price,maten,ratings_id) values ('
 insert into ratings (id, name) values (1, 'geweldig');
 insert into ratings (id, name) values (2, 'top voor de huid'); 
 insert into ratings (id, name) values (3, 'mooie kleuren');
-insert into ratings (id, name) values (4, 'past perfect');  
+insert into ratings (id, name) values (4, 'past perfect'); 
+
+insert into kleuren (id, name) values (1, 'wit');
+insert into kleuren (id, name) values (2, 'zwart'); 
+insert into kleuren (id, name) values (3, 'roze');
+insert into kleuren (id, name) values (4, 'rood');
+insert into kleuren (id, name) values (5, 'meerdere');  
 
 
 insert into platforms (name) values ('instagram'); 
@@ -81,7 +94,7 @@ insert into platforms ( name) values ('kylielips');
   
 
 
- /* 
+ /*
 insert into information (name, description, code,maten) values ('Look-a-like 1', 'taylor swift', '816905633-0',2);
 insert into information (name, description, code,maten) values ('Look-a-like 2', 'ariana grande', '077030122-3',2);
 insert into information (name, description, code,maten) values ('Look-a-like 3', 'gossip girl', '445924201-X',1);
